f27463117e15c5fbf5a1b6a78c44433e9c51c2ea
zaid alhakeem <zaidalhakeem86@gmail.com>
2023-10-09 17:16:52 -0500

    capstone with api key weather

--- store/index.js
+++ store/index.js
@@ -1,3 +1,4 @@
 export { default as Cerebralpalsy } from "./Cerebralpalsy";
 export { default as Home } from "./Home";
 export { default as Contact } from "./Contact";
+export { default as Links } from "./Links";
